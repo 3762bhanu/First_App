760392229505939a5ddc38e01327c58677b0d054
3762bhanu <71490109+3762bhanu@users.noreply.github.com>
2021-01-31 14:04:00 +0530

    Add files via upload

---
+++ APP/app/src/main/java/com/bhanu/myapplication/BirthdayGreetingActivity.kt
@@ -0,0 +1,12 @@
+package com.bhanu.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+    }
+}
